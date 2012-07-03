3275ddc127449f5bb65a08894a5ab839501ab243
Ben Phegan <benphegan@gmail.com>
2012-07-03 12:42:00 +1000

    Add ability to output nuspec files as part of nugetify process

--- NuGet.Extensions/Commands/Nugetify.cs
+++ NuGet.Extensions/Commands/Nugetify.cs
@@ -4,29 +4,32 @@ using System.ComponentModel.Composition;
 using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.Build.Evaluation;
 using NuGet.Commands;
+using NuGet.Common;
 using NuGet.Extensions.GetLatest.MSBuild;
 using NuGet.Extras.Repositories;
-using NuGet.Common;
 
 namespace NuGet.Extensions.Commands
 {
-        [Command("nugetify", "Given a solution, attempts to replace all file references with package references, adding all reqhired" +
-                "packages.config files as it goes.", MinArgs = 1, MaxArgs = 1)]
+    [Command("nugetify", "Given a solution, attempts to replace all file references with package references, adding all reqhired" +
+                         "packages.config files as it goes.", MinArgs = 1, MaxArgs = 1)]
     public class Nugetify : Command
     {
+        private readonly List<ManifestDependency> ManifestDependencies = new List<ManifestDependency>();
         private readonly List<string> _sources = new List<string>();
-        public IPackageRepositoryFactory RepositoryFactory { get; set; }
-        public IPackageSourceProvider SourceProvider { get; set; }
-        private RepositoryAssemblyResolver _resolver;
+        private string _author;
+        private string _copyright;
+        private string _description;
         private IFileSystem _fileSystem;
-
-        [Option("A list of sources to search")]
-        public ICollection<string> Source
-        {
-            get { return _sources; }
-        }
+        private string _id;
+        private string _licenseUrl;
+        private string _releaseNotes;
+        private Boolean _requireLicenseAcceptance;
+        private RepositoryAssemblyResolver _resolver;
+        private string _tags;
+        private string _title;
 
         [ImportingConstructor]
         public Nugetify(IPackageRepositoryFactory packageRepositoryFactory, IPackageSourceProvider sourceProvider)
@@ -36,6 +39,88 @@ namespace NuGet.Extensions.Commands
 
             RepositoryFactory = packageRepositoryFactory;
             SourceProvider = sourceProvider;
+        }
+
+        public IPackageRepositoryFactory RepositoryFactory { get; set; }
+        public IPackageSourceProvider SourceProvider { get; set; }
+
+
+        [Option("A list of sources to search")]
+        public ICollection<string> Source
+        {
+            get { return _sources; }
+        }
+
+        [Option("NuSpec Project URL")]
+        public string ProjectUrl { get; set; }
+
+        [Option("NuSpec LicenseUrl")]
+        public string LicenseUrl
+        {
+            get { return _licenseUrl; }
+            set { _licenseUrl = value; }
+        }
+
+        [Option("NuSpec Icon URL")]
+        public string IconUrl { get; set; }
+
+        [Option("NuSpec tags")]
+        public string Tags
+        {
+            get { return _tags; }
+            set { _tags = value; }
+        }
+
+        [Option("NuSpec release notes")]
+        public string ReleaseNotes
+        {
+            get { return _releaseNotes; }
+            set { _releaseNotes = value; }
+        }
+
+        [Option("NuSpec description")]
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        [Option("NuSpec ID")]
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        [Option("Create NuSpecs for solution")]
+        public Boolean NuSpec { get; set; }
+
+        [Option("NuSpec title")]
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        [Option("NuSpec Author")]
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        [Option("NuSpec RequireLicenseAcceptance (defaults to false)")]
+        public bool RequireLicenseAcceptance
+        {
+            get { return _requireLicenseAcceptance; }
+            set { _requireLicenseAcceptance = value; }
+        }
+
+        [Option(("NuSpec Copyright"))]
+        public string Copyright
+        {
+            get { return _copyright; }
+            set { _copyright = value; }
         }
 
         public override void ExecuteCommand()
@@ -60,6 +145,8 @@ namespace NuGet.Extensions.Commands
                             Console.WriteLine("Processing Project: {0}", simpleProject.ProjectName);
                             var projectFileInfo = new FileInfo(projectPath);
                             var project = new Project(projectPath);
+                            var assemblyOutput = project.GetPropertyValue("AssemblyName");
+
                             var references = project.GetItems("Reference");
 
                             var referenceMappings = ResolveAssembliesToPackagesConfigFile(projectFileInfo, references);
@@ -84,7 +171,7 @@ namespace NuGet.Extensions.Commands
                                         //project.RemoveItem(referenceMatch);
                                         var package = mapping.Value.OrderBy(p => p.GetFiles().Count()).First();
                                         var fileLocation = GetFileLocationFromPackage(package, mapping.Key);
-                                        var newHintPathFull = Path.Combine(solutionRoot.FullName,"packages", package.Id, fileLocation);
+                                        var newHintPathFull = Path.Combine(solutionRoot.FullName, "packages", package.Id, fileLocation);
                                         var newHintPathRelative = String.Format(GetRelativePath(projectPath, newHintPathFull));
                                         //TODO make version available, currently only works for non versioned package directories...
                                         referenceMatch.SetMetadataValue("HintPath", newHintPathRelative);
@@ -101,10 +188,17 @@ namespace NuGet.Extensions.Commands
                                     //TODO We shouldnt need to resolve this twice....
                                     var package = referenceMapping.Value.OrderBy(p => p.GetFiles().Count()).First();
                                     packagesConfig.AddEntry(package.Id, package.Version);
+                                    if (NuSpec)
+                                        ManifestDependencies.Add(new ManifestDependency {Id = package.Id});
                                 }
                                 sharedPackagesRepository.RegisterRepository(packagesConfigFilePath);
                             }
 
+                            //Create nuspec regardless of whether we have added dependencies
+                            if (NuSpec)
+                            {
+                                CreateAndOutputNuSpecFile(assemblyOutput);
+                            }
                         }
                         else
                         {
@@ -115,10 +209,57 @@ namespace NuGet.Extensions.Commands
             }
         }
 
+        private void CreateAndOutputNuSpecFile(string assemblyOutput)
+        {
+            var manifest = new Manifest
+                               {
+                                   Metadata =
+                                       {
+                                           Dependencies = ManifestDependencies,
+                                           Id = _id ?? assemblyOutput,
+                                           Title = _title ?? assemblyOutput,
+                                           Version = "$version$",
+                                           Description = _description ?? assemblyOutput,
+                                           Authors = _author ?? "$author$",
+                                           Tags = _tags ?? "$tags$",
+                                           LicenseUrl = _licenseUrl ?? "$licenseurl$",
+                                           RequireLicenseAcceptance = _requireLicenseAcceptance,
+                                           ReleaseNotes = _releaseNotes ?? "$releasenotes$",
+                                           Copyright = _copyright ?? "$copyright$"
+                                       },
+                                   Files = new List<ManifestFile>
+                                               {
+                                                   new ManifestFile
+                                                       {
+                                                           Source = assemblyOutput + ".dll",
+                                                           Target = "lib"
+                                                       }
+                                               }
+                               };
+
+            string nuspecFile = assemblyOutput + Constants.ManifestExtension;
+
+            try
+            {
+                using (var stream = new MemoryStream())
+                {
+                    manifest.Save(stream, validate: false);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    var content = stream.ReadToEnd();
+                    File.WriteAllText(nuspecFile, RemoveSchemaNamespace(content));
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteError("Could not save file: {0}", nuspecFile);
+                throw;
+            }
+        }
+
         private string GetFileLocationFromPackage(IPackage package, string key)
         {
-            return (from fileLocation in package.GetFiles() 
-                    where fileLocation.Path.ToLowerInvariant().EndsWith(key, StringComparison.OrdinalIgnoreCase) 
+            return (from fileLocation in package.GetFiles()
+                    where fileLocation.Path.ToLowerInvariant().EndsWith(key, StringComparison.OrdinalIgnoreCase)
                     select fileLocation.Path).FirstOrDefault();
         }
 
@@ -127,7 +268,7 @@ namespace NuGet.Extensions.Commands
             // Validate paths.
             Contract.Assert(!String.IsNullOrEmpty(root));
             Contract.Assert(!String.IsNullOrEmpty(child));
-            
+
             // Create Uris
             var rootUri = new Uri(root);
             var childUri = new Uri(child);
@@ -139,11 +280,17 @@ namespace NuGet.Extensions.Commands
             return Uri.UnescapeDataString(relativeUri.ToString()).Replace('/', Path.DirectorySeparatorChar);
         }
 
+        private static string RemoveSchemaNamespace(string content)
+        {
+            // This seems to be the only way to clear out xml namespaces.
+            return Regex.Replace(content, @"(xmlns:?[^=]*=[""][^""]*[""])", String.Empty, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+        }
+
         private Dictionary<string, List<IPackage>> ResolveAssembliesToPackagesConfigFile(FileInfo projectFileInfo, IEnumerable<ProjectItem> references)
         {
             var referenceFiles = new List<string>();
 
-            foreach (var reference in references)
+            foreach (ProjectItem reference in references)
             {
                 //TODO deal with GAC assemblies that we want to replace as well....
                 if (reference.HasMetadata("HintPath"))
@@ -153,7 +300,7 @@ namespace NuGet.Extensions.Commands
                 }
             }
 
-            Dictionary<string, List<IPackage>> results = new Dictionary<string, List<IPackage>>();
+            var results = new Dictionary<string, List<IPackage>>();
             if (referenceFiles.Any())
             {
                 Console.WriteLine("Checking feed for {0} references...", referenceFiles.Count);
@@ -192,6 +339,5 @@ namespace NuGet.Extensions.Commands
             repository.Logger = Console;
             return repository;
         }
-
     }
 }
