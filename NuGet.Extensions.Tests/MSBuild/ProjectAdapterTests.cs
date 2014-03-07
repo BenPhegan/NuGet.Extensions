4f0d0dd99e76dab3b1443f9954bc077432a532f5
GrahamTheCoder <grahamthecoder@gmail.com>
2014-03-07 11:51:24 +0000

    Test version of binary dependency

--- NuGet.Extensions.Tests/MSBuild/ProjectAdapterTests.cs
+++ NuGet.Extensions.Tests/MSBuild/ProjectAdapterTests.cs
@@ -10,6 +10,9 @@ namespace NuGet.Extensions.Tests.MSBuild
     [TestFixture]
     public class ProjectAdapterTests
     {
+        private const string _expectedBinaryDependencyAssemblyName = "Newtonsoft.Json";
+        private const string _expectedBinaryDependencyVersion = "6.0.0.0";
+
         [Test]
         public void ProjectWithDependenciesAssemblyNameIsProjectWithDependencies()
         {
@@ -25,7 +28,18 @@ namespace NuGet.Extensions.Tests.MSBuild
             var binaryReferences = projectAdapter.GetBinaryReferences();
 
             var binaryReferenceIncludeNames = binaryReferences.Select(r => r.IncludeName).ToList();
-            Assert.That(binaryReferenceIncludeNames, Contains.Item("Newtonsoft.Json"));
+            Assert.That(binaryReferenceIncludeNames, Contains.Item(_expectedBinaryDependencyAssemblyName));
+        }
+
+        [Test]
+        public void ProjectWithDependenciesDependsOnNewtonsoftJson6000()
+        {
+            var projectAdapter = CreateProjectAdapter(Paths.ProjectWithDependencies);
+
+            var binaryReferences = projectAdapter.GetBinaryReferences();
+
+            var newtonsoft = binaryReferences.Single(r => r.IncludeName == _expectedBinaryDependencyAssemblyName);
+            Assert.That(newtonsoft.IncludeVersion, Is.EqualTo(_expectedBinaryDependencyVersion));
         }
 
         private static ProjectAdapter CreateProjectAdapter(string projectWithDependencies)
