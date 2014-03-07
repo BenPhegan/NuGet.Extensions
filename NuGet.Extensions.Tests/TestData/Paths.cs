51ced1d231e0c6a65ca16f4eda5210f0a4f125c1
GrahamTheCoder <grahamthecoder@gmail.com>
2014-03-07 11:34:02 +0000

    Add test data

---
+++ NuGet.Extensions.Tests/TestData/Paths.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace NuGet.Extensions.Tests.TestData
+{
+    public static class Paths
+    {
+        public static string RootFolder {get { return Path.Combine(".", "TestData"); }}
+        public static string PackageReferenceSetResolverXml { get { return Path.Combine(RootFolder, "PackageReferenceSetResolverTestData.xml"); } }
+        public static string AdapterTestsSolutionFile { get { return Path.Combine(RootFolder, "TestSolutionForAdapter", "TestSolutionForAdapter.sln"); } }
+    }
+}
