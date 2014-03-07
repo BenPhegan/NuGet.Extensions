0492962beb100766a969cd3c9064cdcb8280d2a9
GrahamTheCoder <grahamthecoder@gmail.com>
2014-03-07 01:08:28 +0000

    Extract tester methods

--- NuGet.Extensions.Tests/ReferenceAnalysers/ReferenceNugetifierBinaryTests.cs
+++ NuGet.Extensions.Tests/ReferenceAnalysers/ReferenceNugetifierBinaryTests.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using Moq;
-using NuGet.Common;
-using NuGet.Extensions.MSBuild;
-using NuGet.Extensions.ReferenceAnalysers;
-using NuGet.Extensions.Tests.Mocks;
 using NUnit.Framework;
 
 namespace NuGet.Extensions.Tests.ReferenceAnalysers
@@ -14,13 +7,11 @@ namespace NuGet.Extensions.Tests.ReferenceAnalysers
     [TestFixture]
     public class ReferenceNugetifierBinaryTests
     {
-        private const string DefaultProjectPath = "c:\\isany.csproj";
-
         [Test]
         public void EmptyProjectHasNoNuggettedDependencies()
         {
-            var nugetifier = BuildNugetifier();
-            var nugettedDependencies = CallNugetifyReferences(nugetifier);
+            var nugetifier = ReferenceNugetifierTester.BuildNugetifier();
+            var nugettedDependencies = ReferenceNugetifierTester.CallNugetifyReferences(nugetifier);
             Assert.That(nugettedDependencies, Is.Empty);
         }
 
@@ -31,31 +22,11 @@ namespace NuGet.Extensions.Tests.ReferenceAnalysers
             var projectWithSingleDependency = ProjectReferenceTestData.ConstructMockProject(new[] {singleDependency.Object});
             var packageRepositoryWithOnePackage = ProjectReferenceTestData.CreateMockRepository();
 
-            var nugetifier = BuildNugetifier(vsProject: projectWithSingleDependency.Object, packageRepository: packageRepositoryWithOnePackage);
-            var nugettedDependencies = CallNugetifyReferences(nugetifier);
+            var nugetifier = ReferenceNugetifierTester.BuildNugetifier(vsProject: projectWithSingleDependency.Object, packageRepository: packageRepositoryWithOnePackage);
+            var nugettedDependencies = ReferenceNugetifierTester.CallNugetifyReferences(nugetifier);
 
             Assert.That(nugettedDependencies, Is.Not.Empty);
             Assert.That(nugettedDependencies.Single().Id, Contains.Substring(ProjectReferenceTestData.PackageInRepository));
-        }
-
-        private static List<ManifestDependency> CallNugetifyReferences(ReferenceNugetifier nugetifier, ISharedPackageRepository sharedPackageRepository = null, string defaultProjectPath = null, List<string> projectReferences = null)
-        {
-            sharedPackageRepository = sharedPackageRepository ?? new Mock<ISharedPackageRepository>().Object;
-            defaultProjectPath = defaultProjectPath ?? DefaultProjectPath;
-            projectReferences = projectReferences ?? new List<string>();
-            return nugetifier.NugetifyReferences(sharedPackageRepository, defaultProjectPath, projectReferences);
-        }
-
-        private static ReferenceNugetifier BuildNugetifier(IFileSystem projectFileSystem = null, IVsProject vsProject = null, PackageReferenceFile packageReferenceFile = null, IPackageRepository packageRepository = null)
-        {
-            var console = new Mock<IConsole>();
-            var projectFileInfo = new FileInfo(DefaultProjectPath);
-            var solutionRoot = new DirectoryInfo("c:\\isAnyFolder");
-            projectFileSystem = projectFileSystem ?? new MockFileSystem(solutionRoot.FullName);
-            vsProject = vsProject ?? new Mock<IVsProject>().Object;
-            packageReferenceFile = packageReferenceFile ?? new PackageReferenceFile(projectFileSystem, solutionRoot.FullName);
-            packageRepository = packageRepository ?? new MockPackageRepository();
-            return new ReferenceNugetifier(console.Object, true, projectFileInfo, solutionRoot, projectFileSystem, vsProject, packageReferenceFile, packageRepository);
         }
     }
 }
---
+++ NuGet.Extensions.Tests/ReferenceAnalysers/ReferenceNugetifierTester.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Moq;
+using NuGet.Common;
+using NuGet.Extensions.MSBuild;
+using NuGet.Extensions.ReferenceAnalysers;
+using NuGet.Extensions.Tests.Mocks;
+
+namespace NuGet.Extensions.Tests.ReferenceAnalysers
+{
+    public class ReferenceNugetifierTester {
+        private const string DefaultProjectPath = "c:\\isany.csproj";
+
+        public static List<ManifestDependency> CallNugetifyReferences(ReferenceNugetifier nugetifier, ISharedPackageRepository sharedPackageRepository = null, string defaultProjectPath = null, List<string> projectReferences = null)
+        {
+            sharedPackageRepository = sharedPackageRepository ?? new Mock<ISharedPackageRepository>().Object;
+            defaultProjectPath = defaultProjectPath ?? DefaultProjectPath;
+            projectReferences = projectReferences ?? new List<string>();
+            return nugetifier.NugetifyReferences(sharedPackageRepository, defaultProjectPath, projectReferences);
+        }
+
+        public static ReferenceNugetifier BuildNugetifier(IFileSystem projectFileSystem = null, IVsProject vsProject = null, PackageReferenceFile packageReferenceFile = null, IPackageRepository packageRepository = null)
+        {
+            var console = new Mock<IConsole>();
+            var projectFileInfo = new FileInfo(DefaultProjectPath);
+            var solutionRoot = new DirectoryInfo("c:\\isAnyFolder");
+            projectFileSystem = projectFileSystem ?? new MockFileSystem(solutionRoot.FullName);
+            vsProject = vsProject ?? new Mock<IVsProject>().Object;
+            packageReferenceFile = packageReferenceFile ?? new PackageReferenceFile(projectFileSystem, solutionRoot.FullName);
+            packageRepository = packageRepository ?? new MockPackageRepository();
+            return new ReferenceNugetifier(console.Object, true, projectFileInfo, solutionRoot, projectFileSystem, vsProject, packageReferenceFile, packageRepository);
+        }
+    }
+}
