c7dded8d2b8921b61ec0a17824b88177b523c084
GrahamTheCoder <grahamthecoder@gmail.com>
2014-03-07 11:47:19 +0000

    Basic tests for ProjectAdapter

---
+++ NuGet.Extensions.Tests/MSBuild/ProjectAdapterTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Build.Evaluation;
+using NuGet.Extensions.MSBuild;
+using NuGet.Extensions.Tests.TestData;
+using NUnit.Framework;
+
+namespace NuGet.Extensions.Tests.MSBuild
+{
+    [TestFixture]
+    public class ProjectAdapterTests
+    {
+        [Test]
+        public void ProjectWithDependenciesAssemblyNameIsProjectWithDependencies()
+        {
+            var projectAdapter = CreateProjectAdapter(Paths.ProjectWithDependencies);
+            Assert.That(projectAdapter.AssemblyName, Is.EqualTo("ProjectWithDependencies"));
+        }
+
+        [Test]
+        public void ProjectWithDependenciesDependsOnNewtonsoftJson()
+        {
+            var projectAdapter = CreateProjectAdapter(Paths.ProjectWithDependencies);
+
+            var binaryReferences = projectAdapter.GetBinaryReferences();
+
+            var binaryReferenceIncludeNames = binaryReferences.Select(r => r.IncludeName).ToList();
+            Assert.That(binaryReferenceIncludeNames, Contains.Item("Newtonsoft.Json"));
+        }
+
+        private static ProjectAdapter CreateProjectAdapter(string projectWithDependencies)
+        {
+            var msBuildProject = new Project(projectWithDependencies, null, null);
+            var projectAdapter = new ProjectAdapter(msBuildProject, "packages.config");
+            return projectAdapter;
+        }
+    }
+}
