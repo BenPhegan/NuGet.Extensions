3e7a9d4b8d7384f482a8996147f16be64f994af5
GrahamTheCoder <grahamthecoder@gmail.com>
2014-03-08 21:47:26 +0000

    Store projects in dictionary for easy access later

--- NuGet.Extensions/MSBuild/SolutionAdapter.cs
+++ NuGet.Extensions/MSBuild/SolutionAdapter.cs
@@ -11,34 +11,30 @@ namespace NuGet.Extensions.MSBuild
         private readonly FileInfo _solutionFile;
         private readonly IConsole _console;
         private readonly ProjectCollection _projectCollection;
+        private readonly Lazy<IDictionary<Guid, ProjectAdapter>> _projectsInSolutionByGuid;
 
         public SolutionAdapter(FileInfo solutionFile, IConsole console)
         {
             _solutionFile = solutionFile;
             _console = console;
             _projectCollection = new ProjectCollection();
+            _projectsInSolutionByGuid = new Lazy<IDictionary<Guid, ProjectAdapter>>(LoadProjectsInSolutionByGuid);
         }
 
         public List<ProjectAdapter> GetProjects()
         {
-            var solution = new Solution(_solutionFile.FullName);
-            var simpleProjectObjects = solution.Projects;
-            var projectAdapters = simpleProjectObjects.Select(p => GetProjectAdapterOrDefault(_solutionFile.Directory, p)).Where(p => p != null).ToList();
-            return projectAdapters;
+            return ProjectsByGuid.Values.ToList();
         }
 
-        private ProjectAdapter GetProjectAdapterOrDefault(DirectoryInfo solutionRoot, SolutionProject simpleProject)
+        private IDictionary<Guid, ProjectAdapter> ProjectsByGuid
         {
-            var projectPath = Path.Combine(solutionRoot.FullName, simpleProject.RelativePath);
-            if (File.Exists(projectPath))
-            {
-                return new ProjectAdapter(projectPath, projectCollection: _projectCollection);
-            }
-            else
-            {
-                _console.WriteWarning("Project: {0} was not found on disk", simpleProject.ProjectName);
-                return null;
-            }
+            get { return _projectsInSolutionByGuid.Value; }
+        }
+
+        private IDictionary<Guid, ProjectAdapter> LoadProjectsInSolutionByGuid()
+        {
+            var solution = new Solution(_solutionFile.FullName);
+            return solution.Projects.Where(ProjectExists).ToDictionary(ProjectGuid, ProjectAdapter);
         }
 
         public void Dispose()
@@ -61,5 +57,30 @@ namespace NuGet.Extensions.MSBuild
             }
             return assembliesReferenced;
         }
+
+        private ProjectAdapter ProjectAdapter(SolutionProject p)
+        {
+            return new ProjectAdapter(GetAbsoluteProjectPath(p), _projectCollection);
+        }
+
+        private static Guid ProjectGuid(SolutionProject p)
+        {
+            return Guid.Parse(p.ProjectGuid);
+        }
+
+        private bool ProjectExists(SolutionProject simpleProject)
+        {
+            var projectPath = GetAbsoluteProjectPath(simpleProject);
+            if (File.Exists(projectPath)) return true;
+
+            _console.WriteWarning("Project: {0} was not found on disk", simpleProject.ProjectName);
+            return false;
+        }
+
+        private string GetAbsoluteProjectPath(SolutionProject simpleProject)
+        {
+            return Path.Combine(_solutionFile.Directory.FullName, simpleProject.RelativePath);
+        }
+
     }
 }
