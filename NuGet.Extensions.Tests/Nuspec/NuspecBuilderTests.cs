d2facdde95b1224cee774659f4c25d6fdd1e466e
GrahamTheCoder <grahamthecoder@gmail.com>
2014-03-15 15:03:57 +0000

    Add a very simple test to nuspec generation

---
+++ NuGet.Extensions.Tests/Nuspec/NuspecBuilderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Moq;
+using NuGet.Common;
+using NuGet.Extensions.Nuspec;
+using NUnit.Framework;
+
+namespace NuGet.Extensions.Tests.Nuspec
+{
+    [TestFixture]
+    public class NuspecBuilderTests
+    {
+        [Test]
+        public void AssemblyNameReplacesNullDescription()
+        {
+            var console = new Mock<IConsole>();
+            const string anyAssemblyName = "any.assembly.name";
+            var nullDataSource = new Mock<INuspecDataSource>().Object;
+
+            var nuspecBuilder = new NuspecBuilder(anyAssemblyName);
+            nuspecBuilder.SetMetadata(nullDataSource, new List<ManifestDependency>());
+            nuspecBuilder.Save(console.Object);
+
+            var nuspecContents = File.ReadAllText(nuspecBuilder.FilePath);
+            Assert.That(nuspecContents, Contains.Substring("<description>" + anyAssemblyName + "</description>"));
+        }
+    }
+}
